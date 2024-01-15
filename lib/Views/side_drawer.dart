0034da31789ad84ebf0932e80d2dc4c9f0b25d61
naqeeb9a <naqeeb9a@gmail.com>
2024-01-15 12:32:26 +0500

    redesigned drawer

--- lib/Views/side_drawer.dart
+++ lib/Views/side_drawer.dart
@@ -409,99 +409,103 @@ class SideDrawerPage extends StatelessWidget {
                 },
                 itemBuilder: (BuildContext context, int index) {
                   LoginModel? loginModel = users[index];
-                  return Container(
-                    padding: const EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10), color: kWhite),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(5),
-                          height: MediaQuery.of(context).size.width * 0.25,
-                          width: MediaQuery.of(context).size.width * 0.25,
-                          decoration: BoxDecoration(
-                            color: const Color(0xffF2F2F2),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: ImageBuilder(
-                            url: loginModel?.owner?.company?.faviconUrl ?? "",
-                            isFit: true,
-                            height: MediaQuery.of(context).size.width * 0.1,
-                            width: MediaQuery.of(context).size.width * 0.1,
-                          ),
-                        ),
-                        const Gap(10),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              CustomText(
-                                text: loginModel?.owner?.fullName ?? '',
-                                fontSize:
-                                    MediaQuery.of(context).size.width * 0.045,
-                                fontWeight: FontWeight.bold,
-                                color: kBlack,
-                                maxLines: 3,
-                                textAlign: TextAlign.left,
-                              ),
-                              const Gap(5),
-                              CustomText(
-                                text: 'Account Managed By',
-                                fontSize:
-                                    MediaQuery.of(context).size.width * 0.03,
-                                fontWeight: FontWeight.w500,
-                                color: const Color(0xFFB2B1B1),
-                                maxLines: 3,
-                                textAlign: TextAlign.left,
-                              ),
-                              const Gap(5),
-                              CustomText(
-                                text: loginModel?.owner?.company?.name ?? '',
-                                fontSize:
-                                    MediaQuery.of(context).size.width * 0.03,
-                                fontWeight: FontWeight.bold,
-                                color: const Color(0xFFB2B1B1),
-                                maxLines: 3,
-                                textAlign: TextAlign.left,
-                              ),
-                            ],
-                          ),
-                        ),
-                        if (index == 0) const Gap(5),
-                        if (index == 0)
+                  return GestureDetector(
+                    onTap: () => onProfileTap(context, users, index),
+                    child: Container(
+                      padding: const EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: kWhite),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
                           Container(
-                            height: 20,
-                            width: 20,
-                            margin: const EdgeInsets.only(bottom: 5),
-                            padding: const EdgeInsets.all(2.0),
+                            padding: const EdgeInsets.all(5),
+                            height: MediaQuery.of(context).size.width * 0.25,
+                            width: MediaQuery.of(context).size.width * 0.25,
                             decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(4),
-                              border: Border.all(
-                                  color: context
-                                      .read<AppThemeCubit>()
-                                      .state
-                                      .primaryColor,
-                                  width: 1),
+                              color: const Color(0xffF2F2F2),
+                              borderRadius: BorderRadius.circular(10),
                             ),
-                            child: Checkbox(
-                              value: index == 0 ? true : false,
-                              fillColor:
-                                  const MaterialStatePropertyAll(kTransparent),
-                              checkColor: context
-                                  .read<AppThemeCubit>()
-                                  .state
-                                  .primaryColor,
-                              side: BorderSide.none,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(4),
-                                  side: BorderSide.none),
-                              onChanged: (value) {},
+                            child: ImageBuilder(
+                              url: loginModel?.owner?.company?.faviconUrl ?? "",
+                              isFit: true,
+                              height: MediaQuery.of(context).size.width * 0.1,
+                              width: MediaQuery.of(context).size.width * 0.1,
                             ),
-                          )
-                      ],
+                          ),
+                          const Gap(10),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                CustomText(
+                                  text: loginModel?.owner?.fullName ?? '',
+                                  fontSize:
+                                      MediaQuery.of(context).size.width * 0.045,
+                                  fontWeight: FontWeight.bold,
+                                  color: kBlack,
+                                  maxLines: 3,
+                                  textAlign: TextAlign.left,
+                                ),
+                                const Gap(5),
+                                CustomText(
+                                  text: 'Account Managed By',
+                                  fontSize:
+                                      MediaQuery.of(context).size.width * 0.03,
+                                  fontWeight: FontWeight.w500,
+                                  color: const Color(0xFFB2B1B1),
+                                  maxLines: 3,
+                                  textAlign: TextAlign.left,
+                                ),
+                                const Gap(5),
+                                CustomText(
+                                  text: loginModel?.owner?.company?.name ?? '',
+                                  fontSize:
+                                      MediaQuery.of(context).size.width * 0.03,
+                                  fontWeight: FontWeight.bold,
+                                  color: const Color(0xFFB2B1B1),
+                                  maxLines: 3,
+                                  textAlign: TextAlign.left,
+                                ),
+                              ],
+                            ),
+                          ),
+                          if (index == 0) const Gap(5),
+                          if (index == 0)
+                            Container(
+                              height: 20,
+                              width: 20,
+                              margin: const EdgeInsets.only(bottom: 5),
+                              padding: const EdgeInsets.all(2.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(4),
+                                border: Border.all(
+                                    color: context
+                                        .read<AppThemeCubit>()
+                                        .state
+                                        .primaryColor,
+                                    width: 1),
+                              ),
+                              child: Checkbox(
+                                value: index == 0 ? true : false,
+                                fillColor: const MaterialStatePropertyAll(
+                                    kTransparent),
+                                checkColor: context
+                                    .read<AppThemeCubit>()
+                                    .state
+                                    .primaryColor,
+                                side: BorderSide.none,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(4),
+                                    side: BorderSide.none),
+                                onChanged: (value) {},
+                              ),
+                            )
+                        ],
+                      ),
                     ),
                   );
                 },
